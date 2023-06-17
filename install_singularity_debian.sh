d1e366b97b7ae5a738c7920725c60febd8335dcb
Ryan Tran <Tranryan13@gmail.com>
2023-06-17 03:29:02 -0500

    Update install_singularity_debian.sh

--- install_singularity_debian.sh
+++ install_singularity_debian.sh
@@ -17,28 +17,30 @@ sudo apt-get install -y \
     fakeroot \
     cryptsetup \
     curl wget git
-   
+
+#Install GO
    export GOVERSION=1.18.4 OS=linux ARCH=amd64  # change this as you need
 
 wget -O /tmp/go${GOVERSION}.${OS}-${ARCH}.tar.gz \
   https://dl.google.com/go/go${GOVERSION}.${OS}-${ARCH}.tar.gz
 sudo tar -C /usr/local -xzf /tmp/go${GOVERSION}.${OS}-${ARCH}.tar.gz
-
+#Add GO to path environment Var
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 source ~/.bashrc
 
+#Clone Repo
 git clone https://github.com/apptainer/apptainer.git
 cd apptainer
 
-git checkout ${APPTAINER_RELEASE_VERSION}
+git checkout v1.1.9
 ./mconfig
 cd ./builddir
 make
 sudo make install
-
 apptainer --version
 
 apt-get install -y autoconf automake libtool pkg-config libfuse-dev zlib1g-dev
+
 #Download SquashFS from Source
 SQUASHFUSEVERSION=0.1.105
 SQUASHFUSEPRS="70 77 81"
@@ -57,10 +59,6 @@ done
 FLAGS=-std=c99 ./configure --enable-multithreading
 make squashfuse_ll
 sudo cp squashfuse_ll /usr/local/libexec/apptainer/bin
-
-export E2E_DOCKER_MIRROR=127.0.0.1:5001
-export E2E_DOCKER_MIRROR_INSECURE=true
-make -C builddir e2e-test
 
 export E2E_DOCKER_MIRROR=127.0.0.1:5001
 export E2E_DOCKER_MIRROR_INSECURE=true
